d826070d7c722035234222d20fc0ede8bd940940
Daniel Redly <redlydp@cardiff.ac.uk>
2022-03-06 22:32:50 +0000

    Templates

--- base/views.py
+++ base/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.template import context
+
+rooms = [
+    {"id": 1, "name": "ReactJS basics"},
+    {"id": 2, "name": "Serverless functions"},
+    {"id": 3, "name": "Styling with TailwindCSS"},
+]
 
 # Create your views here.
 def home(request):
-    return HttpResponse('Welcome home')
+    context={"rooms": rooms}
+    return render(request, "base/home.html", context)
 
-def room(request):
-    return HttpResponse('In the room')
+def room(request, pk):
+    room = None
+    for r in rooms:
+        if r["id"] == int(pk):
+            room = r
+    context={"room": room}
+    return render(request, "base/room.html", context)
