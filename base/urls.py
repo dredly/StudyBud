90fe4d1b0dde2f9cd65a2c54ab2a2893e1e492d1
Daniel Redly <redlydp@cardiff.ac.uk>
2022-03-07 16:40:17 +0000

    CRUD

--- base/urls.py
+++ base/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("room/<str:pk>/", views.room, name="room"),
+    path("create-room/", views.create_room, name="create_room"),
+    path("room/<str:pk>/update/", views.update_room, name="update_room"),
+    path("room/<str:pk>/delete/", views.delete_room, name="delete_room"),
 ]
